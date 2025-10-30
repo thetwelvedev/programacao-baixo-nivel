3db1477287a843131263a7b20805799f6d89d7e9
Leonardo Castro <twelve.programming@gmail.com>
2025-10-29 23:48:28 -0400

    Estudo de Assembly no linux

--- Estudo/hello.asm
+++ Estudo/hello.asm
@@ -1,10 +1,11 @@
 section .data
-    msg db 'Hello World!', 10, 0
+
 section .text
-    global _main
-    extern _printf
-_main:
-    push msg
-    call _printf
-    add esp, 4
-    ret
+
+global _start
+
+_start:
+    ; mov destino, origem -> eax = 1
+    mov eax, 0x1                  ; Só coloquei em hexadecimal 0x1 = 1
+    mov ebx, 0x0                  ; SO o valor de retorno é 0
+    int 0x80                      ; Manda o block de informação e processa os dados
